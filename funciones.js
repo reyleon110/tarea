3d99ecd6f684ae7b611bafff9e20518c05d0035b
reyleon110 <106993500+reyleon110@users.noreply.github.com>
2022-06-17 14:48:25 -0600

    Initial commit

---
+++ funciones.js
@@ -0,0 +1,24 @@
+
+<HTML>
+
+<h1> LOS DIAS DE LA SEMANA</h1>
+<body>
+        </htnl>
+<h2> Lunes </h2>
+<h3>  En la mañana </h3>
+<h4> Los lunes inicio mi semana tomando cafe, normalmente voy al trabajo en bicicleta </h4>
+<h3> En la tarde </h3>
+<h4> En la tarde </h3>
+<h2> Martes </h2>
+<h3> En la mañana </h3>
+<h4> Desayuno frutas y verduras </h4>
+<h3> Miercoles </h3>
+<h4> hice lo mismo que ayer</h4>
+<h3> Jueves </h3>
+<h4> A que no adivinan </h4>
+<h3> Viernes </h3>
+<h4> no hice nada </h4>
+<h3> Sabado </h3>
+<h4> tampoco hoy </h4>
+<h3> Domingo </h3>
+<h4> feliz fin de semana </h4>
